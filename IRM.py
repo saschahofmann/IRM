a8e55a139162fdf44faed89a1789f7508c2ba63c
Unknown <sascha.kelevra@gmail.com>
2017-11-23 16:40:15 +0000

    Introduce average height output for 3 patches

--- IRM.py
+++ IRM.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
+import os
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
@@ -30,7 +31,7 @@ def add_colorbar(im, aspect=20, pad_fraction=0.5, **kwargs):
 n0 = 1.525    # https://us.vwr.com/assetsvc/asset/en_US/id/17619754/contents
 n1 = 1.34
 n2 = 1.37
-n3 =  1.47 # http://onlinelibrary.wiley.com/store/10.1002/cyto.a.20134/asset/20134_ftp.pdf?v=1&t=j809eogl&s=adcee703002c2b2030641711cd89e2b5fe800d94&systemMessage=Wiley+Online+Library+will+be+unavailable+on+Saturday+7th+Oct+from+03.00+EDT+%2F+08%3A00+BST+%2F+12%3A30+IST+%2F+15.00+SGT+to+08.00+EDT+%2F+13.00+BST+%2F+17%3A30+IST+%2F+20.00+SGT+and+Sunday+8th+Oct+from+03.00+EDT+%2F+08%3A00+BST+%2F+12%3A30+IST+%2F+15.00+SGT+to+06.00+EDT+%2F+11.00+BST+%2F+15%3A30+IST+%2F+18.00+SGT+for+essential+maintenance.+Apologies+for+the+inconvenience+caused+.
+n3 =  1.5 # http://onlinelibrary.wiley.com/store/10.1002/cyto.a.20134/asset/20134_ftp.pdf?v=1&t=j809eogl&s=adcee703002c2b2030641711cd89e2b5fe800d94&systemMessage=Wiley+Online+Library+will+be+unavailable+on+Saturday+7th+Oct+from+03.00+EDT+%2F+08%3A00+BST+%2F+12%3A30+IST+%2F+15.00+SGT+to+08.00+EDT+%2F+13.00+BST+%2F+17%3A30+IST+%2F+20.00+SGT+and+Sunday+8th+Oct+from+03.00+EDT+%2F+08%3A00+BST+%2F+12%3A30+IST+%2F+15.00+SGT+to+06.00+EDT+%2F+11.00+BST+%2F+15%3A30+IST+%2F+18.00+SGT+for+essential+maintenance.+Apologies+for+the+inconvenience+caused+.
 w1 = 488
 w2 = 635
 dm = 4 
@@ -44,6 +45,8 @@ I12 = 2783.215
 # Image In-read
 directory = 'Height Measurements/'
 filename = "171110_05.tif"
+if not os.path.exists(directory+filename.replace('.tif', '/')):
+    os.makedirs(directory+filename.replace('.tif', '/'))
 original = io.imread(directory + filename)
 channel1 = original[0] 
 channel2 = original[1] 
@@ -56,7 +59,7 @@ channel2 = original[1]
 #plt.imshow(img)
 
 
-height_img = None
+#height_img = None
 if 'MP' in method :
     import mp
     height_img, I1, I2, channel1, channel2  = mp.MP(channel1, channel2, I11, I12, n0, n1, n2, n3 ,w1, w2, dm)
@@ -67,8 +70,8 @@ if 'normal' in method:
     import minmax
     height_img, I1, I2, channel1, channel2  = minmax.minmaxmethod(channel1, channel2, I11, I12, n0, n1, n3, w1, w2)
     img = Image.fromarray(height_img)   # Creates a PIL-Image object
-    save_name = filename.replace('.tif', '') + '_normal_height.tif'
-    img.save(directory +'/Results/'+ save_name)
+    save_name = filename.replace('.tif', '_') + str(n3) + '_normal_height.tif'
+    img.save(directory +filename.replace('.tif', '/')+ save_name)
 if 'INA' in method:
     import INA
     height_img, I1, I2, channel1, channel2 = INA.INA(channel1, channel2, ina, I11, I12, n0, n1, n3 ,w1, w2)
@@ -83,62 +86,87 @@ if 'INA_MP' in method:
     img.save(directory+'/Results/' + save_name)
     
 import matplotlib.patches as patches
-fig, ax = plt.subplots(1,1)
-int1 = channel1[65:105, 110:170]
-int2 = channel2[65:105, 110:170]
-bg1 = channel1[100:120, 0:40]
-bg2 = channel2[100:120, 0:40]
-core1 = channel1[40:57, 165: 195]
-core2 = channel2[40:57, 165: 195]
-border1 = channel1[8:16, 50:100]
-border2 = channel2[8:16, 50:100]
+x11 = 110
+x12 = 140
+y11= 65
+y12= 100
+int1 = channel1[y11:y12, x11:x12]
+int2 = channel2[y11:y12, x11:x12]
+x21 =250
+x22 = 280
+y21 = 70
+y22 = 100
+bg1 = channel1[y21:y22, x21:x22]
+bg2 = channel2[y21:y22, x21:x22]
+x31 = 120
+x32 = 150
+y31 = 45
+y32 = 55
+#core1 = channel1[40:57, 165: 195]
+#core2 = channel2[40:57, 165: 195]
+#border1 = channel1[8:16, 50:100]
+#border2 = channel2[8:16, 50:100]
 
-ax.axis([np.min(channel1), np.max(channel1), np.min(channel2), np.max(channel2)])
-ax.plot(int1, int2, ls ='', marker = '.',  ms = 0.4, mec = 'black', mfc = 'black' )
-ax.plot(bg1, bg2, ls ='', marker = '.',  ms = 0.4, mec = 'blue', mfc = 'blue' )
-ax.plot(border1, border2, ls = '', marker ='.',  ms = 0.4, mec = '#9e2d04', mfc = '#9e2d04')
-ax.plot(core1, core2, ls = '', marker ='.',  ms = 0.4, mec = 'gray', mfc = 'gray')
-ax.plot(I1,I2, c ='black')
-ax.set_title('Cell refractive index ' + str(n3))
-plt.savefig('irm_n_'+str(n3)+'.jpg', format='jpg', dpi = 500)
+#ax.axis([np.min(channel1), np.max(channel1), np.min(channel2), np.max(channel2)])
+#ax.plot(int1, int2, ls ='', marker = '.',  ms = 0.4, mec = 'black', mfc = 'black' )
+#ax.plot(bg1, bg2, ls ='', marker = '.',  ms = 0.4, mec = 'blue', mfc = 'blue' )
+#ax.plot(border1, border2, ls = '', marker ='.',  ms = 0.4, mec = '#9e2d04', mfc = '#9e2d04')
+#ax.plot(core1, core2, ls = '', marker ='.',  ms = 0.4, mec = 'gray', mfc = 'gray')
+#ax.plot(I1,I2, c ='black')
+#ax.set_title('Cell refractive index ' + str(n3))
+#plt.savefig('irm_n_'+str(n3)+'.jpg', format='jpg', dpi = 500)
 
-#fig2, ax2 = plt.subplots(1,1)
-#ax2.imshow(height_img, cmap ='inferno')    
-#rect = patches.Rectangle((110,65),60,40, fill = False, color = 'black')
-#ax2.add_patch(rect)
-#rect2 = patches.Rectangle((0,100), 40, 20, fill = False, color = 'blue')
-#ax2.add_patch(rect2)
-#rect3 = patches.Rectangle((50,8), 50, 8, fill = False, color = '#9e2d04')
-#ax2.add_patch(rect3)
+av1 = np.mean(height_img[y11:y12, x11:x12])
+av2 = np.mean(height_img[y21:y22, x21:x22])
+av3 = np.mean(height_img[y31:y32, x31:x32])
+std1 = np.std(height_img[y11:y12, x11:x12])
+std2 = np.std(height_img[y21:y22, x21:x22])
+std3 = np.std(height_img[y31:y32, x31:x32])
+mean = (av1+av2+av3)/3
+std = (std1+std2+std3)/3
+fig2, ax2 = plt.subplots(1,1)
+ax2.imshow(height_img, cmap ='inferno') 
+ax2.set_title('Refractive Index: ' + str(n3))   
+rect = patches.Rectangle((x11,y11),x12-x11,y12 - y11, fill = False, color = 'gray')
+ax2.add_patch(rect)
+ax2.annotate(str(av1),xy= (x11, y11), color= 'gray')
+rect2 = patches.Rectangle((x21,y21), x22 -x21, y22 - y21, fill = False, color = 'blue')
+ax2.add_patch(rect2)
+ax2.annotate(str(av2),xy= (x21, y21), color= 'blue')
+rect3 = patches.Rectangle((x31,y31), x32 - x31, y32 - y31, fill = False, color = '#9e2d04')
+ax2.add_patch(rect3)
+ax2.annotate(str(av3),xy= (x31, y31), color ='#9e2d04')
 #rect4 = patches.Rectangle((165, 40), 30, 17, fill = False, color = 'gray')
 #ax2.add_patch(rect4)
-#ax2.axis('off')
-#plt.savefig(filename+'heightpatches.jpg', format ='jpg', dpi = 500)
+ax2.axis('off')
+plt.savefig(directory+filename.replace('.tif', '/')+filename+'_'+str(n3)+'_heightpatches.jpg', format ='jpg', dpi = 500)
 
+print 'Average Height Gray Patch: ', av1, ' +/- ',std1
+print 'Average Height Blue Patch: ', av2, ' +/- ',std2
+print 'Average Height Red Patch: ', av3, ' +/- ',std3
+print 'Mean Height: ', mean, ' +/- ', std
 # Choose field of plot
-height_img = None
-if height_img != None:
-    if True:
-        fig, ax = plt.subplots(2,1, figsize=(16, 16))
-        ax[0].imshow(height_img, cmap = 'inferno')
+if False:
+    fig, ax = plt.subplots(2,1, figsize=(16, 16))
+    ax[0].imshow(height_img, cmap = 'inferno')
 
-        try:
-            while True:
-                xy = plt.ginput(2)
-                xlength = xy[1][0] -xy[0][0]
-                ylength =xy[1][1]- xy[0][1]
-                rect = patches.Rectangle(xy[0], xlength, ylength, fill = False)
-                ax[0].add_patch(rect)
-                section1 = channel1[int(xy[0][1]): int(xy[1][1]), int(xy[0][0]): int(xy[1][0])]
-                section2 = channel2[int(xy[0][1]): int(xy[1][1]), int(xy[0][0]): int(xy[1][0])]
-                ax[1].clear()
-                ax[1].plot(section1,section2, ls = '', marker = ',')
-                ax[1].plot(I1, I2)
-                ax[1].set_aspect(1)
-                ax[1].axis([np.min(channel1), np.max(channel1), np.min(channel2), np.max(channel2)])
-                
-        except KeyboardInterrupt:
-            pass
+    try:
+        while True:
+            xy = plt.ginput(2)
+            xlength = xy[1][0] -xy[0][0]
+            ylength =xy[1][1]- xy[0][1]
+            rect = patches.Rectangle(xy[0], xlength, ylength, fill = False)
+            ax[0].add_patch(rect)
+            section1 = channel1[int(xy[0][1]): int(xy[1][1]), int(xy[0][0]): int(xy[1][0])]
+            section2 = channel2[int(xy[0][1]): int(xy[1][1]), int(xy[0][0]): int(xy[1][0])]
+            ax[1].clear()
+            ax[1].plot(section1,section2, ls = '', marker = ',')
+            ax[1].plot(I1, I2)
+            ax[1].set_aspect(1)
+            ax[1].axis([np.min(channel1), np.max(channel1), np.min(channel2), np.max(channel2)])
+            
+    except KeyboardInterrupt:
+        pass
 
 
 """
